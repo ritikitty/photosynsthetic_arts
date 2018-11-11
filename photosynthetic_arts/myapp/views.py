444f316a78eb9858568a88db23b404b4c2a6a5f7
Orell Hürzeler <orell.huerzeler@gmail.com>
2018-11-11 17:04:19 +0100

    a

--- photosynthetic_arts/myapp/views.py
+++ photosynthetic_arts/myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.contrib.auth.models import User
+#from django.contrib.auth.models import User
 
 def home(request):
     return render(request, 'home.html')
